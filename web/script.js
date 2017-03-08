c62969aa3cc78d2bea2e68d7f0cb6968bca4bde4
BitJakob <jakobseeberg@gmail.com>
2017-03-08 15:03:39 +0100

    Merge branch 'master' of https://github.com/Whazbakh/WebShop2
    # Conflicts:
    #	WebShop2.iml

--- web/script.js
+++ web/script.js
@@ -204,6 +204,24 @@ function signUp() {
 
 function showCostumer(customer) {
     grzegorzSays("Welcome " + customer.name);
+    var Cell1 = document.getElementById("loginTD");
+    Cell1.innerHTML = "";
+    var table = document.createElement("tr");
+    var tr = document.createElement("tr");
+    var Cell2 = document.createElement("td");
+    var Cell3 = document.createElement("td");
+    var btn = document.createElement("BUTTON");
+    var b = document.createTextNode("Logout");
+    btn.appendChild(b);
+    btn.onclick = function () {
+        window.location.reload();
+    };
+    Cell2.innerHTML = "You are logged in as " + customer.name;
+    Cell3.appendChild(btn);
+    tr.appendChild(Cell2);
+    tr.appendChild(Cell3);
+    table.appendChild(tr);
+    Cell1.appendChild(table);
 }
 
 /////////////////////////////////////////////////////
